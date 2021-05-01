a56df697b39187bb134051b82df971ba02537867
jahidrony037 <jahidrony162@gmail.com>
2021-05-01 11:26:49 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,19 @@
+//Undefined vs Null 
+// undefined
+// let pakhi;
+// function add(num1, num2) {
+//     num1 + num2 //if you don't return then it is undefined
+// }
+// function add(x, y) {
+//     x + y;
+// }
+
+// const premik = { name: "kalimoddi", address: "kata-bon" };
+
+// console.log(premik.gfName);
+
+// let fun = undefined;
+// console.log(fun);
+
+let ages = [2, 5, 9];
+console.log(ages[11]);
---
+++ truthandfalse.js
@@ -0,0 +1,16 @@
+//falsy value:
+// 0
+// ""
+// NaN
+// null
+// false
+// undefined
+//Truthy Value:
+// ' ' '0' [] {} truth 
+
+let name = [];
+if (name) {
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+}
