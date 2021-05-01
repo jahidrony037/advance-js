46a1fa4caef56633c8b7e1148cbbfa79f269ffe2
jahidrony037 <jahidrony162@gmail.com>
2021-05-01 17:10:23 +0600

    advanced js

--- null-vs-undefined.js
+++ null-vs-undefined.js
@@ -1,19 +1,23 @@
-//Undefined vs Null 
+// Undefined vs Null 
 // undefined
-// let pakhi;
-// function add(num1, num2) {
-//     num1 + num2 //if you don't return then it is undefined
-// }
-// function add(x, y) {
-//     x + y;
-// }
+let pakhi;
 
-// const premik = { name: "kalimoddi", address: "kata-bon" };
+function add(num1, num2) {
+    num1 + num2 //if you don't return then it is undefined
+}
 
-// console.log(premik.gfName);
+function add(x, y) {
+    x + y;
+}
 
-// let fun = undefined;
-// console.log(fun);
+const premik = { name: "kalimoddi", address: "kata-bon" };
+
+console.log(premik.gfName);
+
+let fun = undefined;
+console.log(fun);
 
 let ages = [2, 5, 9];
 console.log(ages[11]);
+
+//null set kore dite hoy but undefined onk vabei paoa jay jemon kono variable er value set kore  na dile othoba kono array or object thake kono properties access kora jeta oi array or object a exitence kore na
